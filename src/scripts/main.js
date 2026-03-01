4fa93dda49b0fec67a7bde86f47a20ea79d6aea7
Volodymyr Shuper <vshuper@gmail.com>
2026-03-01 17:24:31 +0100

    add tasck solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const images = gallery.querySelectorAll('.gallery__thumb');
+
+images.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const largeImg = document.getElementById('largeImg');
+    const parentLink = image.parentElement;
+    const bigSrc = parentLink.href;
+
+    largeImg.src = bigSrc;
+  });
+});
