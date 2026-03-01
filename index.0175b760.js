6c5bf5cbabfaee998f770bb7c995226491656d25
Volodymyr Shuper <vshuper@gmail.com>
2026-03-01 17:49:45 +0100

    make build

---
+++ index.0175b760.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){var r=e.target.closest("a");r&&(e.preventDefault(),t.src=r.href)});
+//# sourceMappingURL=index.0175b760.js.map
