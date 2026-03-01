3624afabef0ddc20d16d59f988989a7fc4a1218c
Volodymyr Shuper <vshuper@gmail.com>
2026-03-01 17:49:30 +0100

    add tasck solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,12 @@ const gallery = document.querySelector('.gallery');
 const largeImg = document.getElementById('largeImg');
 
 gallery.addEventListener('click', (e) => {
-  if (e.target.classList.contains('gallery__thumb')) {
-    e.preventDefault();
+  const link = e.target.closest('a');
 
-    const parentLink = e.target.parentElement;
-    const bigSrc = parentLink.href;
-    largeImg.src = bigSrc;
+  if (!link) {
+    return;
   }
+  e.preventDefault();
+
+  largeImg.src = link.href;
 });
